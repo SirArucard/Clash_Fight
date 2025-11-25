1296ec53f3828ef47b2e3a99c46484e27ddb6641
Vinícius Prata <108702285+SirArucard@users.noreply.github.com>
2025-11-25 17:53:46 +0000

    saving changes

--- Character.java
+++ Character.java
@@ -7,4 +7,15 @@ public class Character {
     private int vidaAtual;
     private int manaAtual;
 
+    private CharClass modeloClasse;
+
+    public Character (String nome, int xp, int nivel, int vidaAtual, int manaAtual, CharClass classe){
+        this.nome = nome;
+        this.xp = xp;
+        this.nivel = nivel;
+        this.vidaAtual = vidaAtual;
+        this.manaAtual = manaAtual;
+        this.modeloClasse = classe;
+    }
+
 }
---
+++ monsters/Monster.java
@@ -0,0 +1,33 @@
+public abstract class Monster {
+    private String nomeMon;
+    private int vidaMon;
+    private int vidaAtual;
+    private int xpRec;
+    private int PodAtk;
+    
+    public abstract void ataque();
+
+    public Monster(String nomeMon, int vidaMon, int xpRec, int PodAtk) {
+        this.nomeMon = nomeMon;
+        this.vidaMon = vidaMon;
+        this.vidaAtual = vidaMon;
+        this.xpRec = xpRec;
+        this.PodAtk = PodAtk;
+    }
+    public String pegaNome() {
+        return nomeMon;
+    }
+    public int pegaVida() {
+        return vidaMon;
+    }
+    public int pegaXP() {
+        return xpRec;
+    }
+    public int PodAtk() {
+        return PodAtk;
+    }
+    public void recebeDano(int danoRecebido){
+        this.vidaAtual -= danoRecebido;
+    }
+    
+}
